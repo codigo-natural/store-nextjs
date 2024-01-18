072936762b479a112a74fa721e58528f2cae6fa7
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-01-18 06:07:43 -0500

    feat 🎉: adding styles with sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Description } from "./components/home/Description";
-import { Hero } from "./components/home/Hero";
-import { MainProducts } from "./components/home/MainProducts";
+import { Description } from "app/components/home/Description";
+import { Hero } from "app/components/home/Hero";
+import { MainProducts } from "app/components/home/MainProducts";
 
 export default function Home() {
   return (
