bd2999ce475348869b231d367d99c4ff282638db
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-01-18 07:12:30 -0500

    feat 🎉: adding statics images

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,16 @@
+import styles from "./Description.module.sass";
+
 export const Description = () => {
-  return(
-    <section>
-      <h1>Descripción</h1>
+  return (
+    <section className={styles.Description}>
+      <img src="/images/description.jpeg" alt="products marketplace" />
+      <div className={styles.Description__text}>
+        <h2>Description</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          quod.
+        </p>
+      </div>
     </section>
-  )
-}
+  );
+};
