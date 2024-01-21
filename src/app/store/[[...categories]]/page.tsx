fc86b3908ca2a793b6b58431c9d344752096ed17
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-01-21 12:09:13 -0500

    feat 🎉: Implementing Not Found and Global Error Pages

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -7,7 +7,6 @@ interface CategoryProps {
 
 export default function Category(props: CategoryProps) {
   const { categories } = props.params
-  console.log(categories)
   return (
     <h1>Categoria dinámica: {categories}</h1>
   )
