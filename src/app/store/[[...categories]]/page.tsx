dee74e1db0762ab0299bd4f1b1a7bf283b64ad42
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-01-21 16:28:49 -0500

    feat 🎉: Implementing Next.js data fetching patterns

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,14 +1,16 @@
 import { ProductsWrapper } from "app/components/Store/ProductsWrapper/ProductsWrapper"
-import { getProducts } from "app/services/shopify"
+import { getCollections } from "app/services/shopify/collections"
+import { getProducts } from "app/services/shopify/products"
 interface CategoryProps {
   params: {
     categories: string[]
-    searchParams?: string
   }
+  searchParams?: string
 }
 
 export default async function Category(props: CategoryProps) {
   const products = await getProducts()
+  
   const { categories } = props.params
   return (
     <ProductsWrapper products={products} />
