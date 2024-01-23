e1484f6d2ea8e101c04449d93f8c4f24ae3a0841
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-01-23 09:39:03 -0500

    feat 🎉: Implementation and documentation of Fetch and Caché in Next.js

--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -2,7 +2,8 @@ import { env } from "app/config/env"
 
 export const shopifyUrls = {
   products: {
-    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`
+    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`,
+    'mainProducts': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/collections/474570686769/products.json`,
   },
   collections: {
     'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/smart_collections.json`,
