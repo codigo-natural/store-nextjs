e3e3d57dde4300cb6b515283f20201a2f1b4f182
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-01-24 08:51:38 -0500

    feat 🎉: Enhancing SEO for Next.js page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "👨‍💻 Future Store",
+  description: "welcome to the future shop from other mainstrem",
+  keywords: ["eccomerce", "future", "shop"]
+}
 
 export default function Home() {
   return (
