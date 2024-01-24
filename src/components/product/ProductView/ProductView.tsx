bfceb12ccaba4831e9dc5189e10546b24331096c
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-01-24 08:31:21 -0500

    feat 🎉: Dynamic HTML in product description

--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,20 +1,12 @@
-'use client'
-
 import Image from "next/image";
 import { ProductViewItemsOrder } from "./ProductViewItemsOrder";
-import styles from './ProductView.module.sass'
-import { useRouter } from "next/navigation";
+import { SanitizeHTML } from "app/components/shared/SanitizeHTML";
+import styles from "./ProductView.module.sass";
 interface ProductViewProps {
-  product: ProductType
+  product: ProductType;
 }
 
 export const ProductView = ({ product }: ProductViewProps) => {
-  const router = useRouter()
-
-  if(!product) {
-    router.push('/')
-  }
-
   return (
     <main className={styles.ProductView}>
       <section className={styles.ProductView__images}>
@@ -30,14 +22,14 @@ export const ProductView = ({ product }: ProductViewProps) => {
       <section className={styles.ProductView__info}>
         <h1 className={styles.ProductView__info__title}>{product.title}</h1>
         <p className={styles.ProductView__info__category}>{product.tags}</p>
-        <p className={styles.ProductView__info__description}>
+        <SanitizeHTML tag="p" className={styles.ProductView__info__description}>
           {product.description}
-        </p>
+        </SanitizeHTML>
         <span className={styles.ProductView__info__price}>
           $ {product.price}
         </span>
         <ProductViewItemsOrder maxQuantity={product.quantity} />
       </section>
     </main>
-  )
+  );
 };
